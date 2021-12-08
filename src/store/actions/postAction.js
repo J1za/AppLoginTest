a4103df368ff908ee11ff0a8daf5fb7ff733f99d
Vitaliy <ventik6@gmail.com>
2021-12-08 20:32:49 +0200

    fix http-https

--- src/store/actions/postAction.js
+++ src/store/actions/postAction.js
@@ -2,7 +2,7 @@ import { GET_POSTS } from "../types/posts";
 import axios from 'axios'
 
 export const fetchposts = () => async dispatch => {
-    const res = await axios.get('http://jsonplaceholder.typicode.com/posts')
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
     dispatch({
         type: GET_POSTS,
         payload: res.data
